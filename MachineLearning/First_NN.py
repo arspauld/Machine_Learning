9d545e9cf339beb082dd8eab2a2b7957257bc318
arspauld <ars0043@uah.edu>
2018-12-31 20:20:56 -0500

    Big Steps

--- MachineLearning/First_NN.py
+++ MachineLearning/First_NN.py
@@ -1,12 +1,31 @@
-from sklearn import datasets
-import numpy as np
+#ludicrous number of imports
+import numpy
+import h5py
+import os
 
-digit = datasets.load_digits()
+#declare a list to contain CY data
+CY = []
 
+#import the first file
+#CY.append(h5py.File('../../sram flash data/sram/CY_1_HTHV_fresh_50_trials.mat', 'r'))
+file_list = os.listdir('../../sram flash data/sram/')
+
+#import all CY.mat files
+for i in file_list:
+    if i.endswith('.mat') and i.startswith('CY'):
+        data = h5py.File('../../sram flash data/sram/'+i, 'r')
+        data = numpy.array(data.get('data'))
+        CY.append(data.flatten())
+
+#test prints for CY.mat files
+print(CY[0])
+
+
+#normalizing function
 def __sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1/(1+numpy.exp(-x))
 
+#derivative to calculate gradient vector
 def __sigmoid_derivative(x):
-    print('sigmoid derivative\n')
+    return (1/(1+numpy.exp(-x)))-((1/(1+numpy.exp(-x)))**2)
 
-print(__sigmoid(-200))
